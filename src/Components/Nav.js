c9bd156a9bd63971fe660800e5ce42c39cf5435e
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-08-17 17:50:58 -0500

    Update Nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,26 +1,24 @@
-import { Component } from "react";
-import { NavLink, withRouter } from "react-router-dom";
+// import React from "react";
+import { NavLink } from "react-router-dom";
 
-class Nav extends Component {
-  render() {
-    return (
-      <nav className="main-nav">
-        <ul>
-          <li>
-            <NavLink to="/cats">Cats</NavLink>
-          </li>
-          <li>
-            <NavLink to="/dogs">Dogs</NavLink>
-          </li>
-          <li>
-            <NavLink to="/computers">Computers</NavLink>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
+const Nav = () => {
+  return (
+    <nav className="main-nav">
+      <ul>
+        <li>
+          <NavLink to="/cats">Cats</NavLink>
+        </li>
 
-const NavWithRouter = withRouter(Nav);
+        <li>
+          <NavLink to="/dogs">Dogs</NavLink>
+        </li>
 
-export default NavWithRouter;
+        <li>
+          <NavLink to="/computers">Computers</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
